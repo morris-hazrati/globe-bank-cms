a70710dd17beac3431aa5578b783cf8864b59d76
Morris-Desktop <morteza.hazrati@gmail.com>
2021-03-05 13:49:27 +1100

    The firts page created and styled.

--- public/index.php
+++ public/index.php
@@ -1,12 +1,10 @@
 <!doctype html>
 <html>
     <head>
-        <title>
-            
-        </title>
+        <title> Globe Bank </title>
         <meta charset="uft-8">
     </head>
     <body>
-                
+        <h1>Globe Bank: Coming Soon</h1>     
     </body>
 </html>
--- public/staff/index.php
+++ public/staff/index.php
@@ -1,12 +1,27 @@
 <!doctype html>
 <html>
     <head>
-        <title>
-            
-        </title>
+        <title>GBI</title>
         <meta charset="uft-8">
+        <link rel="stylesheet" media="all" href="../stylesheets/staff.css">
     </head>
     <body>
-                
+        <header>
+            <h1>GBI Staff Area</h1>
+        </header> 
+
+        <navigation>
+            <ul>
+                <li><a href="index.php">Menu</a></li>
+            </ul>
+        </navigation> 
+
+        <div id="content">
+        
+        </div>
+
+        <footer>
+            &copy; <?php echo date('Y') ?> Globe Bank
+        </footer>
     </body>
 </html>
