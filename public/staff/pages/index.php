eaeb1b5f0c8e2a1615cf6b141fed2552d01a7f16
Morris-Desktop <morteza.hazrati@gmail.com>
2021-03-05 13:22:16 +1100

    all index.php files put into all folders.

---
+++ public/staff/pages/index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+    <head>
+        <title>
+            
+        </title>
+        <meta charset="uft-8">
+    </head>
+    <body>
+                
+    </body>
+</html>
